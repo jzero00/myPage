261aac0f8b8cfb4fcf0edf75951e434916820767
home <07mrjzero@gmail.com>
2020-10-09 16:15:59 +0900

    board table 설계 v.1.1
    - seq UNIQUE KEY 설정
    - board_seq sequence 생성 쿼리 작성

--- src/main/resources/com/mypage/mybatis/sql/sql.sql
+++ src/main/resources/com/mypage/mybatis/sql/sql.sql
@@ -9,12 +9,18 @@ CREATE TABLE board (
 	seq int4 NOT NULL,
 	writer varchar(100) NOT NULL,
 	title varchar(300) NOT NULL,
-	"content" _bytea NOT NULL,
+	"content" text NOT NULL,
 	regdate date NOT NULL,
-	visible int4 NOT NULL
+	visible int4 NOT NULL,
+	view_cnt int4 NOT NULL,
+	CONSTRAINT board_un UNIQUE (seq)
 );
 
 
 -- public.board foreign keys
 
 ALTER TABLE public.board ADD CONSTRAINT board_fk FOREIGN KEY (mcode) REFERENCES menu(mcode);
+
+
+-- board_seq 생성
+CREATE SEQUENCE board_seq START 1;
