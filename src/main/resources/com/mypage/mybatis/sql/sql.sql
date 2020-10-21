9eae9900185adb1539e2f8d5747d6870538dcce9
jzeor <07jzero@gmail.com>
2020-10-21 10:29:57 +0900

    member table 설계

--- src/main/resources/com/mypage/mybatis/sql/sql.sql
+++ src/main/resources/com/mypage/mybatis/sql/sql.sql
@@ -3,6 +3,7 @@
 -- Drop table
 
 -- DROP TABLE board;
+-- DROP TABLE member;
 
 CREATE TABLE board (
 	mcode varchar(30) NOT NULL,
@@ -15,6 +16,25 @@ CREATE TABLE board (
 	view_cnt int4 NOT NULL,
 	CONSTRAINT board_un UNIQUE (seq)
 );
+
+CREATE TABLE member (
+	id varchar(30) NOT NULL,
+	"password" varchar NOT NULL,
+	"name" varchar(30) NOT NULL,
+	nickname varchar NOT NULL,
+	"enable" varchar NOT NULL,
+	authority varchar NOT NULL,
+	CONSTRAINT member_pk PRIMARY KEY (id)
+);
+
+-- Column comments
+
+COMMENT ON COLUMN public."member".id IS 'ID';
+COMMENT ON COLUMN public."member"."password" IS 'password';
+COMMENT ON COLUMN public."member"."name" IS '이름';
+COMMENT ON COLUMN public."member".nickname IS '닉네임';
+COMMENT ON COLUMN public."member"."enable" IS '활성화여부';
+COMMENT ON COLUMN public."member".authority IS '권한';
 
 
 -- public.board foreign keys
